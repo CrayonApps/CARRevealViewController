777579f4702d51f88d942f922616c9260944e49a
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2014-04-06 00:44:25 +0900

    Initial Commit

---
+++ CARRevealViewControllerDemo/CARAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CARAppDelegate.h
+//  CARRevealViewControllerDemo
+//
+//  Created by Yamazaki Mitsuyoshi on 4/6/14.
+//  Copyright (c) 2014 CrayonApps.inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CARAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CARRevealViewControllerDemo/CARViewController.h
@@ -0,0 +1,13 @@
+//
+//  CARViewController.h
+//  CARRevealViewControllerDemo
+//
+//  Created by Yamazaki Mitsuyoshi on 4/6/14.
+//  Copyright (c) 2014 CrayonApps.inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CARViewController : UIViewController
+
+@end
